6ec141df9d592f5d8db8119fd24c43ca45bed54b
gabriel <dgabrielfof@gmail.com>
2024-04-18 09:57:46 -0300

    recycle: refactor: making the update script with size adjustment more readable and limiting component updates to only when there are changes in the number of elements

--- src/components/card_area/card_area.js
+++ src/components/card_area/card_area.js
@@ -3,43 +3,93 @@ export class CardArea{
     this.elementsArray = elementsArray;
     this.childWidth = 193;
     this.childHeight = 260;
-    this.maxChildrens = 2
+    this.currentWidth = 1;
+    this.currentMaxHeight = 1;
+    this.columnsNumber = 1;
+    this.rowsNumber = 1;
+    this.maxChildrens = 1;
+    this.currentPage = 1;
+    this.pagesNumber = 1; 
   }
 
   create() {
     const cardArea = document.createElement("div");
     cardArea.classList.add("card_area");
 
-    for (let child = 0; child < this.maxChildrens; child++) {
-      cardArea.appendChild(this.elementsArray[child])
-    }
+    this._injectChilds(cardArea)
     
     this.resizeObserver.observe(cardArea);
     return cardArea
   }
 
+
+
   resizeObserver = new ResizeObserver(entries => {
     for (let entry of entries) {
-      const currentWidth = entry.contentRect.width;
-      const columnsNumber = Math.floor(currentWidth/this.childWidth)
-      entry.target.style.gridTemplateColumns = `repeat(${columnsNumber}, auto)`;
+      const maxChildrens = this.maxChildrens;
+      this._setElementWidth(entry);
+      this._updateColumnsNumber();
       const style = window.getComputedStyle(entry.target);
       if (style) {
-        let currentHeight = style.getPropertyValue('max-height').match(/\d+/g).map(Number)[0]
-        if (currentHeight) {
-          currentHeight
-        }
-        const rowsNumber = Math.floor(currentHeight/this.childHeight)
-        entry.target.style.gridTemplateRowns = `repeat(${rowsNumber}, auto)`;
-        this.maxChildrens = columnsNumber * rowsNumber;
-        const cardArea = document.querySelector(".card_area");
-        cardArea.innerHTML = ""
-        for (let child = 0; child < this.maxChildrens; child++) {
-          if (child<this.elementsArray.length) {
-            cardArea.appendChild(this.elementsArray[child])
-          }
-        }
+        this._setElementMaxWidth(entry);
+        this._updateRowsNumber();
+      }
+      this._setMaxChildrens();
+      if (this.maxChildrens !== maxChildrens) {
+        console.log("update")
+        this._setGridTemplateStyle(entry);
+        this._updateCardAreaChilds();
       }
     }
   });
+  
+  _updateCardAreaChilds() {
+    const cardArea = document.querySelector(".card_area");
+    cardArea.innerHTML = "";
+    this._injectChilds(cardArea);
+  }
+
+  _injectChilds(parent) {
+    for (let child = 0; child < this.maxChildrens; child++) {
+      if (child < this.elementsArray.length) {
+        parent.appendChild(this.elementsArray[child]);
+      }
+    }
+  }
+
+  _setElementWidth(element) {
+    this.currentWidth = element.contentRect.width;
+    return this.currentWidth
+  }
+
+  _updateColumnsNumber() {
+    this.columnsNumber = Math.floor(this.currentWidth/this.childWidth)
+    return this.columnsNumber
+  }
+
+  _setElementMaxWidth(element) {
+    const style = window.getComputedStyle(element.target)
+    if (style) {
+      this.currentMaxHeight = style.getPropertyValue('max-height').match(/\d+/g).map(Number)[0];
+      return  this.currentMaxHeight     
+    }
+  }
+
+  _setGridTemplateStyle(element) {
+    element.target.style.gridTemplateColumns = `repeat(${this.columnsNumber}, auto)`;
+    element.target.style.gridTemplateRowns = `repeat(${this.rowsNumber}, auto)`;
+  }
+
+  _updateRowsNumber() {
+    this.rowsNumber = Math.floor(this.currentMaxHeight/this.childHeight)
+    return this.rowsNumber
+  }
+
+  _updateMaxChildrens() {
+    this.maxChildrens = this.columnsNumber * this.rowsNumber;
+  }
+
+  _setMaxChildrens() {
+    this.maxChildrens = this.columnsNumber * this.rowsNumber;
+  }
 }
