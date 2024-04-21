05ae905eee794064f480bd64c18af68c9d53e9ae
gabriel <dgabrielfof@gmail.com>
2024-04-21 18:03:10 -0300

    :recycle: refactor:  extracting component-only functions to class methods

--- src/components/card_area/card_area.js
+++ src/components/card_area/card_area.js
@@ -4,6 +4,7 @@ export class CardArea{
   constructor(elementsArray) {
     this.elementsArray = elementsArray;
     this.cardArea = document.createElement("div");
+    this.cardLimit = 1;
   }
 
   create() {
@@ -16,5 +17,34 @@ export class CardArea{
   update(newArray, length) {
     this.cardArea.innerHTML = "";
     ViewService.injectChilds(this.cardArea, newArray, length)
+  }
+
+  getColumnsNumber(cardAreaWidth, cardSizeSelectedWidth) {
+    return Math.floor(cardAreaWidth / cardSizeSelectedWidth)
+  }
+
+  getRowsNumber(cardAreaHeight, cardSizeSelectedHeight) {
+    return Math.floor(cardAreaHeight / cardSizeSelectedHeight)
+  }
+
+  getMaxChildrens(columns, rows) {
+    return columns * rows;
+  }
+
+  setGridTemplateStyle(columns, rows){
+    this.cardArea.style.gridTemplateColumns = `repeat(${columns}, auto)`
+    this.cardArea.style.gridTemplateRowns = `repeat(${rows}, auto)`
+  }
+
+  getItemsToShow(currentPage, childrenLimit, arrayOfChildrens) {
+    const firstElement = childrenLimit * (currentPage - 1)
+    const lastElement = childrenLimit * currentPage
+    return arrayOfChildrens.slice(firstElement, lastElement);
+  }
+
+  setPagesNumber(completeArray, arrayLimit) {
+    const reminder = completeArray.length % arrayLimit;
+    const whole = Math.floor(completeArray.length / arrayLimit)
+    return reminder == 0 ? whole : whole + 1;
   }
 }
